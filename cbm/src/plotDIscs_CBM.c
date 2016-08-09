2d0b5becbd6b33d84486975caa6112dc90e939f3
mc78 <mc78@outlook.de>
2016-08-09 17:22:31 +0200

    trying to optimize cbm target

--- cbm/src/plotDIscs_CBM.c
+++ cbm/src/plotDIscs_CBM.c
@@ -8,9 +8,9 @@ int sintable[] = {
     #include "sintable.h"*/
 };
 
-int sinpos[DOTS] = {0*DISTANCE,1*DISTANCE,2*DISTANCE,3*DISTANCE,
+uint8_t sinpos[DOTS] = {0*DISTANCE,1*DISTANCE,2*DISTANCE,3*DISTANCE,
                     4*DISTANCE,5*DISTANCE,6*DISTANCE,7*DISTANCE};
-int slopepos[DOTS] = {0*SLOPEDIST, 1*SLOPEDIST, 2*SLOPEDIST, 3*SLOPEDIST,
+uint8_t slopepos[DOTS] = {0*SLOPEDIST, 1*SLOPEDIST, 2*SLOPEDIST, 3*SLOPEDIST,
                         4*SLOPEDIST, 5*SLOPEDIST, 6*SLOPEDIST, 7*SLOPEDIST};
 
 void initDiscPlotter(size_t len, size_t seqlen, size_t max){
@@ -31,24 +31,32 @@ CBM_discRenderer(Renderer_t *renderer, Texture_t *texture, drawingObjTargetRect_
 
 void plotDiscs(Renderer_t *renderer, Texture_t *texture, drawingObjTargetRect_t *renderRect){
     static uint8_t i, ix2;
-    int sinx, siny;
+    int sinx,
+        siny,
+        offsetx = (SCREEN_WIDTH  + renderRect->w) / 2,
+        offsety = (SCREEN_HEIGHT + renderRect->h) / 2,
+        sinpos_current, slopepos_current;
+
     VIC.spr_hi_x = 0;
 
     for (i = 0, ix2=0; i<DOTS; ++i,ix2+=2){
-        sinx = (SCREEN_WIDTH/2)+sintable[sinpos[i]]-renderRect->w/2;
-        siny = (SCREEN_HEIGHT/2)+sintable[sinpos[i]+64]-renderRect->h/2;
-        sinx/=2;
-        siny/=2;
+        sinpos_current = sinpos[i];
+        slopepos_current = slopepos[i];
 
-        renderRect->x = sinx; //(SCREEN_WIDTH/2)+sintable[sinpos[i]]-renderRect->w/2;
+        sinx = offsetx+sintable[sinpos_current]/2;
+        siny = offsety+sintable[sinpos_current+64]/2;
+        //sinx/=2;
+        //siny/=2;
+
+        __AX__ = renderRect->x = sinx; //(SCREEN_WIDTH/2)+sintable[sinpos[i]]-renderRect->w/2;
         asm("ldx %v", ix2);
         asm("sta %w,x", 0xd000);
-        __AX__ =  renderRect->y = (SCREEN_HEIGHT/2)+sintable[sinpos[i]+64]-renderRect->h/2;
+        __AX__ = siny;//(SCREEN_HEIGHT/2)+sintable[sinpos[i]+64]-renderRect->h/2;
         asm("ldx %v", ix2);
         asm("sta %w,x", 0xd001);
 
-        sinpos[i] = (sinpos[i]+slopetable[slopepos[i]])%256;
-        slopepos[i] = (slopepos[i]+1)%256;
+        sinpos[i] = (sinpos_current+slopetable[slopepos_current])%256;
+        slopepos[i] = (slopepos_current+1)%256;
 
         if ((renderRect->x>>8) > 0) VIC.spr_hi_x |= powerof2table[i];
         //SDL_RenderCopy(renderer, texture, 0, &renderRect);
